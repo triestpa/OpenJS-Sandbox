22bdd5558591575b27d54046e648802ab788bf67
triestpa <patrick.triest@gmail.com>
2017-07-24 00:08:34 +0700

    Add iframe sandbox script

---
+++ sandbox.js
@@ -0,0 +1,35 @@
+/**
+  * Sandbox iframe script
+  * We run the user-inputted code by contructing a function.
+  * This is a better practice than using eval(), but is still very insecure.
+  * Don't do this in your main app, only use this technique in sandboxed iframes.
+ */
+
+var context = null // Store current message context in iframe window scope
+
+/** Listen for messages and run received code */
+window.addEventListener('message', function (e) {
+  context = e
+  try {
+    new Function(e.data)() // Construct and run function from message content
+  } catch (err) {
+    postMessage('error', { message: err.message })
+  }
+});
+
+/** Override default console.log */
+window.console.log = (message, body) => {
+  postMessage('log', { message, body });
+}
+
+/** Override default console.error */
+window.console.error = (error) => {
+  postMessage('error', { message: error.message })
+}
+
+/** Post message back to main application */
+function postMessage(type, log) {
+  context.source.postMessage({
+    type, body: JSON.stringify(log)
+  }, context.origin);
+}
