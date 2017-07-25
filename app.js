aedb9270d0964ea16cc6fa5cad4ba6964b94d4a6
triestpa <patrick.triest@gmail.com>
2017-07-25 15:40:59 +0700

    Move iframe behavior into class

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ if (navigator.serviceWorker) {
 const logListId = 'log-list'
 const sandboxId = 'sandbox'
 
+// Show basic "hello world" code by default
 const initialValue =
 `function hello () {
   console.log("Hello world!");
@@ -16,22 +17,23 @@ const initialValue =
 
 hello()`
 
+// Store app-wide vars in a global object, to avoid any potential global scope conflicts
+const app = {
+  editor: null,
+  sandbox: null
+}
+
 /** Handler for when the DOM is fully loaded */
-document.addEventListener("DOMContentLoaded", function(){
-  //setupMonacoEditor()
-  //setupAceEditor()
-
-  // params = new URLSearchParams(window.location.search)
-  // console.log(params.get('editor'))
-
+document.addEventListener('DOMContentLoaded', () => {
   /** Listen for messages from the sandboxed iframe */
   window.addEventListener('message', (e) => {
-    // Get the sandbox iframe
-    let frame = document.getElementById(sandboxId)
+    if (app.sandbox) {
+      let frame = app.sandbox.iframeElement
 
-    // Verify that the message is from the sandbox iframe
-    if (e.origin === "null" && frame && e.source === frame.contentWindow) {
-      handleLogMessage(e.data)
+      // Verify that the message is from the sandbox iframe
+      if (e.origin === "null" && frame && e.source === frame.contentWindow) {
+        handleLogMessage(e.data)
+      }
     }
   })
 })
@@ -48,33 +50,6 @@ function handleLogMessage (log) {
   }
 }
 
-/** Run the editor code in the sandboxed iframe */
-function runCode (code) {
-  clearChilden(logListId)
-
-  try {
-    let frame = document.getElementById(sandboxId)
-    frame.contentWindow.postMessage(code, '*')
-  } catch (err) {
-    console.error(err)
-  }
-}
-
-/** Add the target sandbox iframe to the DOM */
-function addSandbox () {
-  const iframe = document.createElement('iframe')
-  iframe.srcdoc = '<html><head><script src="sandbox.js"></script></head></html>';;
-  iframe.id = sandboxId
-  iframe.sandbox = 'allow-scripts'
-  document.body.appendChild(iframe)
-}
-
-/** Remve the sandboxed iframe from the DOM, halting any ongoing sandboxed script execution. */
-function removeSandbox () {
-  const elem = document.getElementById(sandboxId)
-  elem.parentNode.removeChild(elem)
-}
-
 /** Clear all child nodes of a given HTML element */
 function clearChilden(id) {
   const logList = document.getElementById(id)
@@ -86,6 +61,7 @@ function clearChilden(id) {
 
 /** Add a log message to the DOM */
 function addLog (log) {
+  console.log(log)
   const pre = document.createElement('pre')
   pre.appendChild(document.createTextNode(log.message))
 
@@ -95,6 +71,60 @@ function addLog (log) {
 
   document.getElementById(logListId).appendChild(pre)
 }
+
+/** Class to manage iframe code sandbox */
+class Sandbox {
+  /** Contruct sandbox class and check for ID conflicts */
+  constructor (elementId) {
+    if (document.getElementById(elementId)) {
+      throw new Error(`Only one sandbox with id ${elementId} allowed at a time.`)
+    }
+
+    this.id = elementId
+  }
+
+  /** Run code in sandbox */
+  run (code) {
+    if (!this.iframeElement) {
+      throw new Error('Must start sandbox before runing code.')
+    }
+
+    try {
+      // Send code to sandbox iframe
+      this.iframeElement.contentWindow.postMessage(code, '*')
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  /** Add the iframe sandbox to the DOM */
+  start () {
+    const iframe = document.createElement('iframe')
+    iframe.srcdoc = '<html><head><script src="sandbox.js"></script></head></html>';;
+    iframe.id = this.id
+    iframe.sandbox = 'allow-scripts'
+    document.body.appendChild(iframe)
+    this.iframeElement = iframe
+
+    return new Promise((resolve, reject) => {
+      // Wait for iframe to initialize.
+      setTimeout(() => {
+        resolve()
+      }, 200)
+    })
+  }
+
+  /** Remove the iframe sandbox from the DOM */
+  stop () {
+    const elem = document.getElementById(this.id)
+    if (elem) { elem.parentNode.removeChild(elem) }
+  }
+}
+
+/**
+ * SECTION
+ * Editor Configuration and Behavior
+ */
 
 /** Return a promise that will resolve once the provided script has been imported */
 function importScript (url) {
@@ -110,22 +140,22 @@ function importScript (url) {
 
 /** Generate a function to run code from the provider editor */
 function onEditorChangeListener(editor) {
-  const onEditorChange = (change) => {
-    if (document.getElementById(sandboxId)) {
-      removeSandbox() // Remove existing sandbox
+  const onEditorChange = async (change) => {
+    if (app.sandbox) {
+      app.sandbox.stop() // Remove existing sandbox
     }
 
-    addSandbox() // Add sandbox iframe
-    setTimeout(() => { // Wait for iframe to initialize
-      runCode(editor.getValue())
-    }, 200)
+    app.sandbox = new Sandbox(sandboxId)
+    await app.sandbox.start()
+    clearChilden(logListId)
+    app.sandbox.run(editor.getValue())
   }
 
   return onEditorChange
 }
 
 
-/** Abstract editor class */
+/** Abstract Editor class */
 class Editor {
   constructor () { }
 
@@ -134,11 +164,29 @@ class Editor {
     return importScript(this.dependency)
   }
 
-  // Abstract methods to be overriden in subclasses
-  loadEditor (elementId, initialValue) { }
-  getValue () { }
-  setOnChangeListener (listener) { }
+  /**
+   * Provide abstract methods with default error handling behavior
+   */
 
+  loadEditor (elementId, initialValue) {
+    if (!elementId) {
+      throw new Error('Must provide element id for editor.')
+    }
+
+    this.elementId = elementId
+  }
+
+  getValue () {
+    if (!this.editor) {
+      throw new Error('Must load editor before getting value.')
+    }
+  }
+
+  setOnChangeListener (listener) {
+    if (!this.editor) {
+      throw new Error('Must load editor before setting change listener.')
+    }
+  }
 }
 
 class MonacoEditor extends Editor {
@@ -148,6 +196,7 @@ class MonacoEditor extends Editor {
   }
 
   async loadEditor(elementId, initialValue) {
+    super.loadEditor(elementId, initialValue)
     await this.loadDependency()
     this.editor = await new Promise((resolve, reject) => {
       require.config({ paths: { 'vs': 'vs' }});
@@ -157,18 +206,20 @@ class MonacoEditor extends Editor {
           language: 'javascript',
           theme: 'vs-dark',
           fontSize: '14px',
-        });
+        })
+
         resolve(editor)
       })
     })
   }
 
   setOnChangeListener(listener) {
-    console.log('editor', this.editor)
+    super.setOnChangeListener(listener)
     this.editor.onDidChangeModelContent(listener)
   }
 
   getValue() {
+    super.getValue()
     return this.editor.getValue()
   }
 }
@@ -180,6 +231,7 @@ class AceEditor extends Editor {
   }
 
   async loadEditor(elementId, initialValue) {
+    super.loadEditor(elementId, initialValue)
     await this.loadDependency()
     this.editor = ace.edit('editor')
     this.editor.setOptions({
@@ -192,29 +244,43 @@ class AceEditor extends Editor {
   }
 
   setOnChangeListener(listener) {
+    super.setOnChangeListener(listener)
     this.editor.getSession().on('change', listener)
   }
 
   getValue() {
+    super.getValue()
     return this.editor.getValue()
   }
 }
 
+/**
+ * SECTION
+ * UI-linked Functions
+ */
+
 /** Setup Monaco Editor */
 async function setupMonacoEditor () {
-  const editor = new MonacoEditor()
-  await editor.loadEditor('editor', initialValue)
-  editor.setOnChangeListener(onEditorChangeListener(editor))
+  document.getElementById('select-editor').style.visibility = 'hidden';
+  app.editor = new MonacoEditor()
+  await app.editor.loadEditor('editor', initialValue)
+  app.editor.setOnChangeListener(onEditorChangeListener(app.editor))
 }
 
 /** Setup ACE editor  */
 async function setupAceEditor () {
-  const editor = new AceEditor()
-  await editor.loadEditor('editor', initialValue)
-  editor.setOnChangeListener(onEditorChangeListener(editor))
+  document.getElementById('select-editor').style.visibility = 'hidden';
+  app.editor = new AceEditor()
+  await app.editor.loadEditor('editor', initialValue)
+  app.editor.setOnChangeListener(onEditorChangeListener(app.editor))
 }
 
+/** Save editor contents to local JS file */
 function saveAsFile () {
+  const text = app.editor.getValue()
   const blob = new Blob([text], {type: "text/javascript;charset=utf-8"});
-  saveAs(blob, filename+".js");
+  saveAs(blob, 'sandbox-code.js');
 }
+
+
+
